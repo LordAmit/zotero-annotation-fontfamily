8f4a8f6b5786544ef8bfe9e83f4cc2dfe3833236
Amit Seal Ami <lordamit@gmail.com>
2025-12-19 02:46:54 -0500

    working

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
-import { BasicTool } from "zotero-plugin-toolkit";
 import Addon from "./addon";
 import { config } from "../package.json";
 import * as hooks from "./hooks";
 
-const basicTool = new BasicTool();
-
 const addonInstance = new Addon();
 (addonInstance as any).hooks = hooks;
-
-// Set this globally first!
 (globalThis as any).addon = addonInstance;
 
 if (!Zotero[config.addonInstance]) {
